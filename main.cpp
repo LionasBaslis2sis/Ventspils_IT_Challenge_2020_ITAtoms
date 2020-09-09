7f662b41f49fea6039d63776f5ff6db932f0e4e8
LionasBaslis2sis <arnasvaic7@gmail.com>
2020-09-09 21:50:14 +0300

    gui & time
    Basic GUI for the simulation requirements. Simulation now keeps track of time.

--- main.cpp
+++ main.cpp
@@ -3,18 +3,66 @@
 #include <bits/stdc++.h>
 
 int main(const int argc, const char** argv) {
-	// window setup	
+	/// window setup	
 	sf::VideoMode mode(1024, 768);
 	const char* name = "Simulation";
 	int style = sf::Style::Titlebar | sf::Style::Close;
 	sf::RenderWindow window(mode, name, style);
 	sf::Event event;
-	// loop
+	/// resources
+	tgui::Font font("res/consolas.ttf");
+	tgui::Theme theme("res/Black.txt");
+	tgui::Theme::setDefault(&theme);
+	/// gui
+	tgui::Gui gui(window);
+	// info label
+	std::stringstream sstream;
+	auto info = tgui::Label::create();
+	info->setPosition(mode.width - info->getSize().x - 10, 10);
+	gui.add(info);
+	// team info label
+	auto team_info = tgui::Label::create();
+    team_info->setText("ITAtoms: Arnas Vaicekauskas, Justas Komza and tas 10okas kazkoks. Mentor: Ramune Simkuviene");
+    team_info->setPosition(10, mode.height - team_info->getSize().y - 10);
+    //team_info->setTextSize(12);
+    gui.add(team_info);
+	// start button
+	auto start = tgui::Button::create();
+    start->setPosition(10, 10);
+    start->setText("Start");
+    start->setSize(72, 32);
+    start->connect("pressed", [](){});
+    gui.add(start);
+	// stop button
+	auto stop = tgui::Button::create();
+    stop->setPosition(92, 10);
+    stop->setText("Stop");
+    stop->setSize(72, 32);
+    stop->connect("pressed", [](){});
+    gui.add(stop);
+	// exit button
+	auto exit = tgui::Button::create();
+    exit->setPosition(174, 10);
+    exit->setText("Exit");
+    exit->setSize(72, 32);
+    exit->connect("pressed", [&](){ window.close(); });
+    gui.add(exit);
+	/// loop setup
+	sf::Clock clock;
+	long t = 0;
+	/// loop
 	while(window.isOpen()) {
+		t = clock.getElapsedTime().asMilliseconds();
+		sstream << std::fixed << std::setprecision(2) << t / 1000.0f;
+		info->setText("time: " + sstream.str());
+		sstream.str("");
+		info->setPosition(mode.width - info->getSize().x - 10, 10);
 		while(window.pollEvent(event)) {
 			if(event.type == sf::Event::Closed) window.close();
+			gui.handleEvent(event);
 		}
 		window.clear();
+		gui.draw();
 		window.display();
 	}
 	return EXIT_SUCCESS;
